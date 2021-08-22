b617cf6f3aea74ded8ad745f35f4fcf29457e91d
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-08-22 23:34:42 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login_Page/Login.jsx";
+import SignUp from "./Login_Page/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Switch>
+          <Route path="/signup" exact component={SignUp} />
+          <Route exact path="/login" exact component={Login} />
+        </Switch>
+      </Router>
     </div>
   );
 }
