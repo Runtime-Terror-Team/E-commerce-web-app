0a04607f636ed4d23340d0a9c398270e078975a8
Yashvi <yashvigarg330@gmail.com>
2021-08-23 08:29:20 +0530

    done

--- src/components/Helmet.jsx
+++ src/components/Helmet.jsx
@@ -1,27 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 import PropTypes from "prop-types";
-=======
-import PropTypes from 'prop-types'
-import React from 'react'
->>>>>>> 0ab11363d77cfa258bb5335f6454684aaee64fea
 
 const Helmet = (props) => {
   document.title = "RTT Store - " + props.title;
 
-<<<<<<< HEAD
   return <div>{props.children}</div>;
 };
-=======
-    document.title = 'RTT Store - ' + props.title
-
-    return (
-        <div>
-            {props.children}
-        </div>
-    )
-}
->>>>>>> 0ab11363d77cfa258bb5335f6454684aaee64fea
 
 Helmet.propTypes = {
   title: PropTypes.string.isRequired,
