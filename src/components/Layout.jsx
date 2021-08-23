f8e716f2169395dca4fcc24be459015e6103ba1b
Yashvi <yashvigarg330@gmail.com>
2021-08-23 18:43:32 +0530

    build

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import Routes from '../routes/Routes'
+import React from "react";
+import { HashRouter as BrowserRouter, Route } from "react-router-dom";
+import Routes from "../routes/Routes";
 // import Header from './Header'
-import Footer from './Footer'
-
-
-
+import Footer from "./Footer";
 
 const Layout = () => {
-    return (
-        <BrowserRouter>
-            <Route render={props => (
-                <div>
-                    {/* <Header {...props}/> */}
-                    <div className="container">
-                        <div className="main">
-                            <Routes/>
-                        </div>
-                    </div>
-                    <Footer/>
-                </div>
-            )}/>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <div>
+            {/* <Header {...props}/> */}
+            <div className="container">
+              <div className="main">
+                <Routes />
+              </div>
+            </div>
+            <Footer />
+          </div>
+        )}
+      />
+    </BrowserRouter>
+  );
+};
 
-export default Layout
+export default Layout;
